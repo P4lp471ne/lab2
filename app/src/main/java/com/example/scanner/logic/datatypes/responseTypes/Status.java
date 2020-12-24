ba4ae954f3f97fe14e0a6c381616324b052bdff1
Andrew <dovgopliy200259@gmail.com>
2020-12-24 02:54:56 +0000

    status enum upd

--- app/src/main/java/com/example/scanner/logic/datatypes/responseTypes/Status.java
+++ app/src/main/java/com/example/scanner/logic/datatypes/responseTypes/Status.java
@@ -1,8 +1,23 @@
 package com.example.scanner.logic.datatypes.responseTypes;
 
 public enum Status {
-    NEW,
-    IN_PROGRESS,
-    FINISH,
-    CANCELED;
+    NEW("new"),
+    IN_PROGRESS("in_progress"),
+    FINISH("finish"),
+    CANCELED("canceled");
+
+    private String title;
+
+    Status(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
 }
